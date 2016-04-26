cfd68c49434ed969ce3d18193423abd4cec4926a
Santiago Ferreira <san650@gmail.com>
2016-04-26 13:51:35 -0300

    Merge tests/assets into /public for tests

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-test-assets'
+  name: 'ember-test-assets',
+
+  treeForPublic: function(originalTree) {
+    var tree = this._super.treeForPublic(originalTree);
+
+    if (!this.app.tests) {
+      return tree;
+    }
+
+    if (!tree) {
+      return new Funnel('tests/assets');
+    }
+
+    return mergeTrees(['public', 'tests/assets'], { overwrite: true });
+  }
 };
